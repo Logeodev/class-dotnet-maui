add80db47a3d25d8ce061580195a6a65ff2a17ea
leo.logel <leo.logel@sdea.fr>
2024-12-16 14:24:53 +0100

    Added TTT cli project (refactor original code)

---
+++ TicTacToeCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Common;
+
+namespace TicTacToeCLI;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        new Game().RunGame();
+    }
+}
